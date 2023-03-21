8c2d48597487a0fb199d7f6308b34e09a367dcee
TusharSatija <115475413+TusharSatija@users.noreply.github.com>
2023-03-21 22:49:18 +0530

    Add files via upload

---
+++ l.js
@@ -0,0 +1,38 @@
+//normal for loop 
+
+for(let i=0;i<10;i++)
+{
+    console.log(i);
+}
+
+//0---9 value we get 
+
+
+//for each loop;
+
+let a=[1,2,3,4,5,6,7,8,9,10];
+
+for(itm of a)
+{
+    console.log(itm);
+}
+
+
+let fruits=['orange','mango','banana','kiwi'];
+
+for(fruit of fruits)
+{
+    console.log(fruit);
+}
+
+
+let person={
+    name:"satija",
+    age:20,
+    isAdult:true
+};
+
+for(index in person)
+{
+    console.log(index);
+}
